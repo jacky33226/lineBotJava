85bb489454ba1d8a08b8d3b989f526d43bc39962
jacky <jacky@dafacloud.com>
2021-06-14 16:16:40 +0800

    echo message with line sdk

--- src/main/java/com/tingyu/linebot/controller/CallbackController.java
+++ src/main/java/com/tingyu/linebot/controller/CallbackController.java
@@ -1,12 +1,6 @@
 package com.tingyu.linebot.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.tingyu.linebot.model.CallbackConfig;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.stream.Collectors;
 
 /**
  * @author jacky
@@ -16,24 +10,7 @@ import java.util.stream.Collectors;
 @RestController
 public class CallbackController {
 
-    @PostMapping("/callback")
-    public String callback(HttpServletRequest request, CallbackConfig callbackConfig){
 
-        try {
-
-            System.out.println(JSONObject.toJSONString(request.getHeaderNames()));
-
-            String test = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-            System.out.println(test);
-
-            System.out.println(JSONObject.toJSONString(callbackConfig));
-
-            return "success";
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-        return "success";
-    }
 
 
 }
